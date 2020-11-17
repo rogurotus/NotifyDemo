4726b38212bec1fa1eb9a627786c8cd45411ad6c
rogurotus <rogurotus@gmail.com>
2020-11-17 15:18:34 +0700

    Создание канала уведомления

--- app/src/main/java/a/notifydemo/MainActivity.java
+++ app/src/main/java/a/notifydemo/MainActivity.java
@@ -2,17 +2,38 @@ package a.notifydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
+    NotificationManager notificationManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        createNotificationChannel("a.notifydemo.news",
+                "NotifyDemo News", "Example News Channel");
+    }
+
+    protected void createNotificationChannel(String id, String name, String description) {
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+
+        channel.setDescription(description);
+        channel.enableLights(true);
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+        channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(channel);
     }
 
     public void sendNotification(View view) {}
